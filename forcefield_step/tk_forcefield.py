d52a5dd88a8a071b8e473c4368f71a715acdb50b
Paul Saxe <paul@thesaxes.net>
2019-09-18 15:06:41 -0400

    Switched to local forcefield file in data.
    And to using parameters object.

--- forcefield_step/tk_forcefield.py
+++ forcefield_step/tk_forcefield.py
@@ -3,19 +3,15 @@
 """The graphical part of a Forcefield step"""
 
 import seamm
-import forcefield_step
-import os.path
+import seamm_widgets as sw
+import Pmw
 import tkinter as tk
-import tkinter.filedialog as tk_filedialog
 import tkinter.ttk as ttk
 
 
 class TkForcefield(seamm.TkNode):
-    """The node_class is the class of the 'real' node that this
-    class is the Tk graphics partner for
+    """The graphical part of a forcefield step
     """
-
-    node_class = forcefield_step.Forcefield
 
     def __init__(
         self,
@@ -44,52 +40,6 @@ class TkForcefield(seamm.TkNode):
             h=h
         )
 
-    def create_dialog(self):
-        """Create the dialog!"""
-
-        self.dialog = tk.Toplevel(master=self.toplevel)
-        self.dialog.transient(self.toplevel)
-        self.dialog.withdraw()
-        self.dialog.title('Forcefield...')
-
-        frame = ttk.Frame(self.dialog)
-        frame.pack(side='top', fill=tk.BOTH, expand=1)
-
-        ff_file_label = ttk.Label(frame, text='Forcefield:')
-        self.ff_file_widget = ttk.Entry(frame, width=30)
-        self.ff_file_widget.insert(0, self.node.ff_file)
-        ff_file_button = ttk.Button(frame, text='...', command=self.ff_file_cb)
-        ff_file_label.grid(row=0, column=0)
-        self.ff_file_widget.grid(row=0, column=1)
-        ff_file_button.grid(row=0, column=2)
-
-        button_box = ttk.Frame(self.dialog)
-        button_box.pack(side='bottom', expand=1, fill=tk.X)
-        ok_button = ttk.Button(button_box, text="OK", command=self.handle_ok)
-        ok_button.grid(row=0, column=0, sticky=tk.W)
-        help_button = ttk.Button(
-            button_box, text="Help", command=self.handle_help
-        )
-        help_button.grid(row=0, column=1)
-        cancel_button = ttk.Button(
-            button_box, text="Cancel", command=self.handle_cancel
-        )
-        cancel_button.grid(row=0, column=2, sticky=tk.E)
-        button_box.grid_columnconfigure(0, weight=1)
-        button_box.grid_columnconfigure(1, weight=1)
-        button_box.grid_columnconfigure(2, weight=1)
-
-    def ff_file_cb(self):
-        """Present the file dialog for getting the forcefield"""
-        name = tk_filedialog.askopenfilename(
-            parent=self.dialog,
-            filetypes=[('all files', '.*'), ('forcefield files', '.frc')],
-            initialdir=os.path.dirname(self.node.ff_file)
-        )
-        if name != '':
-            self.ff_file_widget.delete(0, tk.END)
-            self.ff_file_widget.insert(0, name)
-
     def right_click(self, event):
         """Probably need to add our dialog...
         """
@@ -103,37 +53,54 @@ class TkForcefield(seamm.TkNode):
         """Present a dialog for editing the Forcefield flowchart
         """
 
-        if not self.dialog:
-            self.create_dialog()
-
-        self._tmp = {'dialog': self.dialog}
-        self.previous_grab = self.dialog.grab_current()
-        self.dialog.grab_set()
-        self.dialog.deiconify()
-        self.dialog.attributes('-topmost', True)
-
-    def handle_ok(self):
-        self.dialog.grab_release()
-        self.dialog.attributes('-topmost', False)
+        self.dialog = Pmw.Dialog(
+            self.toplevel,
+            buttons=('OK', 'Help', 'Cancel'),
+            defaultbutton='OK',
+            master=self.toplevel,
+            title='Edit Forcefield parameters',
+            command=self.handle_dialog
+        )
         self.dialog.withdraw()
-        if self.previous_grab is not None:
-            self.previous_grab().grab_set()
-            self.previous_grab = None
-        self.node.ff_file = self.ff_file_widget.get()
 
-    def handle_help(self):
-        print('Help')
-        self.dialog.grab_release()
-        self.dialog.attributes('-topmost', False)
-        self.dialog.withdraw()
-        if self.previous_grab is not None:
-            self.previous_grab().grab_set()
-            self.previous_grab = None
+        frame = ttk.Frame(self.dialog.interior())
+        frame.pack(expand=tk.YES, fill=tk.BOTH)
+        self['frame'] = frame
 
-    def handle_cancel(self):
-        self.dialog.grab_release()
-        self.dialog.attributes('-topmost', False)
-        self.dialog.withdraw()
-        if self.previous_grab is not None:
-            self.previous_grab().grab_set()
-            self.previous_grab = None
+        # Create the widgets and grid them in
+        P = self.node.parameters
+        row = 0
+        widgets = []
+        for key in P:
+            self[key] = P[key].widget(frame)
+            widgets.append(self[key])
+            self[key].grid(row=row, column=0, sticky=tk.EW)
+            row += 1
+
+        sw.align_labels(widgets)
+
+        self.dialog.activate(geometry='centerscreenfirst')
+
+    def handle_dialog(self, result):
+
+        if result is None or result == 'Cancel':
+            self.dialog.deactivate(result)
+            return
+
+        if result == 'Help':
+            # display help!!!
+            return
+
+        if result != "OK":
+            self.dialog.deactivate(result)
+            raise RuntimeError(
+                "Don't recognize dialog result '{}'".format(result)
+            )
+
+        self.dialog.deactivate(result)
+
+        # Shortcut for parameters
+        P = self.node.parameters
+
+        for key in P:
+            P[key].set_from_widget()
