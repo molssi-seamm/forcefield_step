c911c47ce3621076fce651b039a7034e8b4c1730
Paul Saxe <paul@thesaxes.net>
2020-09-24 09:24:33 -0400

    Fixing flake8 issues

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ project_root = os.path.dirname(cwd)
 # version is used.
 sys.path.insert(0, project_root)
 
-import forcefield_step
+import forcefield_step  # noqa: E402
 
 # -- General configuration ---------------------------------------------
 
@@ -51,7 +51,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The encoding of source files.
-#source_encoding = 'utf-8-sig'
+# source_encoding = 'utf-8-sig'
 
 # The master toctree document.
 master_doc = 'index'
