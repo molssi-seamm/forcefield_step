ce4ff381650b641f354a715e69bc9ee356b7c95f
Eliseo Marin <meliseo@vt.edu>
2021-02-18 00:58:21 -0500

    Update forcefield parameters with preinstalled typing engines

--- forcefield_step/forcefield_parameters.py
+++ forcefield_step/forcefield_parameters.py
@@ -10,79 +10,34 @@ import seamm
 
 logger = logging.getLogger(__name__)
 
-
-# Get the list of available forcefields
-path = pkg_resources.resource_filename(__name__, 'data/')
-
 # Check if we can use OpenKIM
 if importlib.util.find_spec('kim_query') is None:
     forcefields = []
 else:
     forcefields = ['OpenKIM']
 
-logger.debug('Looking for forcefields at ' + path)
-with os.scandir(path) as it:
-    for entry in it:
-        if not entry.name.startswith('.') and entry.is_file():
-            logger.debug('   ' + entry.name)
-            ffname, ext = os.path.splitext(entry.name)
-            if ext == '.frc':
-                forcefields.append(entry.name)
+plugin_manager = seamm.PluginManager("org.molssi.seamm.atom_typers")
+available_atomtyping_engines= []
 
-forcefields = sorted(forcefields)
+logger.debug('Looking for installed forcefield plugins')
 
+for group in plugin_manager.groups():
+    for plugin in plugin_manager.plugins(group):
+        available_atomtyping_engines.append(plugin)
+
+available_atomtyping_engines = tuple(available_atomtyping_engines)
 
 class ForcefieldParameters(seamm.Parameters):
     """The control parameters for forcefields"""
 
-    parameters = {
-        "task": {
-            "default": 'setup forcefield',
+        "atomtyping_engine": {
+            "default": available_atomtyping_engines[0],
             "kind": "enumeration",
             "default_units": "",
-            "enumeration": (
-                'setup forcefield',
-                'assign forcefield to structure',
-            ),
+            "enumeration": available_atomtyping_engines,
             "format_string": "s",
-            "description": "What to do:",
-            "help_text": "What to do with the forcefield."
-        },
-        "forcefield_file": {
-            "default": forcefields[0],
-            "kind": "enumeration",
-            "default_units": "",
-            "enumeration": tuple(forcefields),
-            "format_string": "s",
-            "description": "Forcefield Repository:",
-            "help_text": "The forcefield repository or file to use."
-        },
-        "forcefield": {
-            "default": "default",
-            "kind": "enumeration",
-            "default_units": "",
-            "enumeration": ('default',),
-            "format_string": "s",
-            "description": "Forcefield:",
-            "help_text": "The forcefield with the file."
-        },
-        "elements": {
-            "default": "",
-            "kind": "periodic table",
-            "default_units": None,
-            "enumeration": None,
-            "format_string": "",
-            "description": "Elements:",
-            "help_text": "The elements to include."
-        },
-        "potentials": {
-            "default": "",
-            "kind": "enumeration",
-            "default_units": "",
-            "enumeration": ('will be replaced',),
-            "format_string": "s",
-            "description": "Interatomic Potentials:",
-            "help_text": "The interatomic potentials to use."
+            "description": "The atomtyping tool to be used:",
+            "help_text": "The atomtyping tool to use."
         },
     }
 
