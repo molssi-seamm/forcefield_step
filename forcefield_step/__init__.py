9dc08fa85d03891fa5551058e5155f1d86a8e724
Paul Saxe <paul@thesaxes.net>
2022-02-03 04:31:41 -0500

    blackened

--- forcefield_step/__init__.py
+++ forcefield_step/__init__.py
@@ -9,7 +9,9 @@ A step for choosing the default forcefield.
 # the package.
 
 from forcefield_step.forcefield import Forcefield  # noqa: F401
-from forcefield_step.forcefield_parameters import ForcefieldParameters  # noqa: F401,E501
+from forcefield_step.forcefield_parameters import (  # noqa: F401
+    ForcefieldParameters,
+)
 from forcefield_step.forcefield_step import ForcefieldStep  # noqa: F401
 from forcefield_step.tk_forcefield import TkForcefield  # noqa: F401
 
@@ -17,8 +19,8 @@ from forcefield_step.tk_forcefield import TkForcefield  # noqa: F401
 from ._version import get_versions
 
 __author__ = """Paul Saxe"""
-__email__ = 'psaxe@molssi.org'
+__email__ = "psaxe@molssi.org"
 versions = get_versions()
-__version__ = versions['version']
-__git_revision__ = versions['full-revisionid']
+__version__ = versions["version"]
+__git_revision__ = versions["full-revisionid"]
 del get_versions, versions
